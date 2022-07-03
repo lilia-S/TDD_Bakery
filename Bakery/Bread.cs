952b5059dbe88abab568b58a8846e685bf158599
liliia87 <lilia.krivelyova@gmail.com>
2022-07-02 22:54:58 -0700

    BreadChoose_GetTwoCost10_true

--- Bakery/Bread.cs
+++ Bakery/Bread.cs
@@ -15,8 +15,24 @@ public class Bread: IBakery
   {
     if(this.Amount == 1)
       return this.Price;
-    if(this.Amount == 2)
-      return this.Price*2;
+    if(this.Amount % 3 != 0)
+    {
+      var calcAmount = this.Amount/3;
+      var left = this.Amount - calcAmount;
+      var price = this.Price*2;
+      if(left == 1)
+      {
+        return price*calcAmount+this.Price;
+      }
+      
+      return price*(calcAmount+1);
+    }
+    else
+    {
+      var calcAmount = this.Amount/3;
+      var price = this.Price*2;
+      return calcAmount*price;
+    }
 
     return 0;
   }
