031100a69dfee3066a7f31ca805cc46530f99cac
liliia87 <lilia.krivelyova@gmail.com>
2022-07-03 11:02:07 -0700

    add totalCost for file Program.cs

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -9,7 +9,12 @@ namespace Bakery
             Bread bread = new Bread(1, 5);
             var breadPrice = bread.calculatePrice();
             Pastry pastry = new Pastry(1, 2);
+            Console.WriteLine("Hi, Here is your order --------------------------------------->");
+            Console.WriteLine("You choose Bread: {0}", bread.Amount);
+            Console.WriteLine("You choose Pastry: {0}", pastry.Amount);
             var pastryPrice = pastry.calculatePrice();
+            var totalCost = breadPrice + pastryPrice;
+            Console.WriteLine("The total cost of order is: {0}", totalCost);
         }
     }
 }
