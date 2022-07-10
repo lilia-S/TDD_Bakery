985641f23a3ee549f846b4e2fe37b7e8d4576db2
liliia87 <lilia.krivelyova@gmail.com>
2022-07-10 10:05:23 -0700

    rewrite Main method in Program.cs function

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -6,9 +6,15 @@ namespace Bakery
     {
         static void Main(string[] args)
         {   
-            Bread bread = new Bread(1, 5);
+            Console.WriteLine("Hi, our bread costs 5$ for each. Enter you the amount you want to order.");
+            var breadAmount = Int32.Parse(Console.ReadLine());
+            Bread bread = new Bread(breadAmount, 5);
             var breadPrice = bread.calculatePrice();
-            Pastry pastry = new Pastry(1, 2);
+            
+            
+            Console.WriteLine("Hi, our pastry costs 2$ for each. Enter you the amount you want to order.");
+            var pastryAmount = Int32.Parse(Console.ReadLine());
+            Pastry pastry = new Pastry(pastryAmount, 2);
             Console.WriteLine("Hi, Here is your order --------------------------------------->");
             Console.WriteLine("You choose Bread: {0}", bread.Amount);
             Console.WriteLine("You choose Pastry: {0}", pastry.Amount);
